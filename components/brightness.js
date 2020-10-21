3efbcd00a6aa4eff7a7d13bb71eb68d91c16893d
Keith Dahlby <keith@newbo.co>
2020-10-21 10:22:08 -0500

    Avoid setting brightness without permissions

--- components/brightness.js
+++ components/brightness.js
@@ -2,21 +2,26 @@ import React, { useEffect, useState } from 'react';
 import {View, Text, Button } from 'react-native';
 import * as Brightness from 'expo-brightness';
 
-
-export default function App() {
+export default function BrightnessDemo() {
+  const [permission, setPermission] = useState(false);
  
   useEffect(() => {
     (async () => {
       const { status } = await Brightness.requestPermissionsAsync();
       if (status === 'granted') {
-        Brightness.setSystemBrightnessAsync(.5);
+        setPermission(true)
       }
     })();
-  })
+  }, [])
     
   
   
   function danceMode() {
+    if (!permission) {
+      console.log('Cannot dance mode')
+      return;
+    }
+
     for(let i = 0; i < 1000; i++) {
       Brightness.setBrightnessAsync(Math.random());
     }
